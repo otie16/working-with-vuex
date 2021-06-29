d26530678b1197481fb77cc6bc7eaaa8f8ef580c
otie16 <biggieestone@gmail.com>
2021-06-29 11:44:26 +0100

    add new

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,36 @@
 <template>
   <div id="app">
-    <product-list-one></product-list-one>
-    <product-list-two></product-list-two>
+      <product-list-one v-bind:products="products"></product-list-one>
+      <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
 export default {
-  name: "app",
-  data() {
-    return {
-      products: [
-        { name: "Banana Skin", price: 20 },
-        { name: "Shiny Star", price: 40 },
-        { name: "Green Shells", price: 60 },
-        { name: "Red Shells", price: 80 },
-      ],
-    };
-  },
-};
+    name: 'app',
+    components: {
+        'product-list-one': ProductListOne,
+        'product-list-two': ProductListTwo
+    },
+    data () {
+        return {
+            products: [
+                {name: 'Banana Skin', price: 20},
+                {name: 'Shiny Star', price: 40},
+                {name: 'Green Shells', price: 60},
+                {name: 'Red Shells', price: 80}
+            ]
+        }
+    }
+}
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+body{
+    font-family: Ubuntu;
+    color: #555;
 }
 </style>
+© 2021 GitHub, Inc.
